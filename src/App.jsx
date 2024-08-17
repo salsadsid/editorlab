76f4cd2fed386aba596935982cc07ac685904a38
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2024-08-17 12:18:06 +0600

    ☀ Project setup: editorlab \n � Implemented eslint vite configuration \n � Setup tailwind, material-tailwind, react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@material-tailwind/react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Editor Lab</h1>
+      <Button>Enhance</Button>
+    </>
+  );
+}
+
+export default App;
